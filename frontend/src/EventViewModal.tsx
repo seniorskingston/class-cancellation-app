a85b7b7cd6eb82040b112fbfb5d655d814eb3fbf
seniorskingston <rebeccam@seniorskingston.ca>
2025-10-22 14:09:48 -0400

    Update EventViewModal.tsx

--- frontend/src/EventViewModal.tsx
+++ frontend/src/EventViewModal.tsx
@@ -1,3 +1,15 @@
 // Placeholder file - not used
-export default null;
+import React from 'react';
+
+interface EventViewModalProps {
+  isOpen: boolean;
+  onClose: () => void;
+  event: any;
+}
+
+const EventViewModal: React.FC<EventViewModalProps> = () => {
+  return null;
+};
+
+export default EventViewModal;
 
